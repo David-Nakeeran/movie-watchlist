8f159985141c63ca0d05a0f040713c45f1542204
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2023-12-30 15:22:56 +0000

    completed project

--- index.js
+++ index.js
@@ -1,16 +1,130 @@
 'use strict';
 
-const btn = document.getElementById('search-btn');
-const inputF = document.getElementById('text-field');
-const form = document.getElementById('movie');
+const inputField = document.getElementById('text-field');
+const searchForm = document.getElementById('movie');
+const container = document.getElementById('container');
 
-movie.addEventListener('submit', (e) => {
-    e.preventDefault();
-    test();
+
+const moviesArr = [];
+
+
+let watchlistArr = JSON.parse(localStorage.getItem('watchlist')) || [];
+
+
+// replaces icon class
+// adds films to watchlist array, removes duplicates and saves to local storage
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.filmid) {
+        const id = e.target.dataset.filmid;
+
+        const elementActionIcon = document.querySelector(`[data-filmid='${id}']`);
+        elementActionIcon.classList.replace('fa-circle-plus', 'fa-circle-check');
+
+        for(const item of moviesArr) {
+            if(item.imdbID === id) {
+                watchlistArr.push(item);
+            };
+        };
+
+        watchlistArr = watchlistArr.reduce((acc, current) => {
+            const duplicates = acc.find(item =>  {
+            return item.imdbID === current.imdbID;
+        });
+            if(!duplicates) {
+                return acc.concat(current);
+            }else {
+                return acc;
+            };
+        }, []);
+        
+        setLocal();
+
+    };
 });
 
-async function test() {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=2ec4c37c&s=${inputF.value}`);
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    removeChildNodes(container);
+    
+    if(inputField.value === null || inputField.value === '') {
+        removeChildNodes(container);
+        container.innerHTML = `
+        <div class="start-exploring">
+            <p>Unable to find what you're looking for. Please try another search.<p>
+        </div>    
+        `
+
+    } else {
+        getFilmDetails();
+    }
+    
+});
+
+// fetches data from api and pushes to movie array calls renderFilm function
+async function getFilmDetails() {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=2ec4c37c&s=${inputField.value}&type=movie`);
     const data = await response.json();
-    console.log(data.Search);
-}
+
+    const filmId = getImdbID(data.Search);
+
+    for(const item of filmId) {
+        const response2 = await fetch(`http://www.omdbapi.com/?apikey=2ec4c37c&i=${item}`);
+        const data2 = await response2.json();
+        moviesArr.push(data2)
+    };
+    renderFilm(moviesArr, container);
+};
+
+// renders html to page
+function renderFilm(films, container) {
+    let htmlFeed = '';
+
+    for(const film of films) {
+        htmlFeed += `
+        <div class="film-container">
+            <div class="img-container">
+                <img src="${film.Poster}" class="film-poster" alt="poster of film">
+            </div>
+            <div class="film-details">
+                <div class="film-title-container">
+                    <p class="film-title">${film.Title}</p>
+                    <img class="rating-star" src="./images/star.png" alt="image of yellow star">
+                    <p class="film-rating">${film.imdbRating}</p>
+                </div>
+                <div class="film-info">
+                    <p class="film-typography">${film.Runtime}</p>
+                    <p class="film-typography">${film.Genre}</p>
+                    <i class="fa-solid fa-circle-plus" data-filmid="${film.imdbID}"></i>
+                    <p class="watchlist-text film-typography">Watchlist</p>
+                </div>      
+                <p class="film-plot">${film.Plot}</p>
+            </div>
+        </div>
+        `
+    };
+    container.innerHTML = htmlFeed;
+
+};
+
+// returns array of movie imdbIDs
+function getImdbID(filmArr) {
+        const movieId = filmArr.map((movie) => {
+        return movie.imdbID;
+    });
+    return movieId;
+};
+
+// saves to local storage
+function setLocal() {
+    localStorage.setItem('watchlist', JSON.stringify(watchlistArr));
+};
+
+// checks for child nodes - removes if true
+function removeChildNodes(parent) {
+    if(parent.children.length > 0) {
+        while(parent.firstChild) {
+            parent.removeChild(parent.lastChild);
+        };
+    };
+};
---
+++ watchlist.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const watchContainer = document.getElementById('watch-container');
+
+
+let addedToWatchlist = JSON.parse(localStorage.getItem('watchlist'));
+
+if(addedToWatchlist.length === 0) {
+    renderEmptyWatchlist();
+    
+} else {
+    renderFilm(addedToWatchlist, watchContainer);
+};
+
+
+const watchlistElements = document.querySelectorAll('.fa-circle-plus');
+for(const item of watchlistElements) {
+    item.classList.replace('fa-circle-plus', 'fa-circle-minus');
+};
+
+
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.filmid) {
+        const id = e.target.dataset.filmid;
+
+        const el = e.target.closest('.film-container');
+        el.remove();
+        
+        addedToWatchlist = addedToWatchlist.filter((element) => {
+            
+            return element.imdbID !== id;
+        });
+
+        
+    
+    };
+
+    if(addedToWatchlist.length === 0) {
+        renderEmptyWatchlist();
+        
+    };
+        
+   localStorage.setItem('watchlist', JSON.stringify(addedToWatchlist))
+});
+
+
+function renderFilm(films, container) {
+    let htmlFeed = '';
+
+    for(const film of films) {
+        htmlFeed += `
+        <div class="film-container">
+            <div class="img-container">
+                <img src="${film.Poster}" class="film-poster" alt="poster of film">
+            </div>
+            <div class="film-details">
+                <div class="film-title-container">
+                    <p class="film-title">${film.Title}</p>
+                    <img class="rating-star" src="./images/star.png" alt="image of yellow star">
+                    <p class="film-rating">${film.imdbRating}</p>
+                </div>
+                <div class="film-info">
+                    <p class="film-typography">${film.Runtime}</p>
+                    <p class="film-typography">${film.Genre}</p>
+                    <i class="fa-solid fa-circle-plus" data-filmid="${film.imdbID}"></i>
+                    <p class="watchlist-text film-typography">Watchlist</p>
+                </div>      
+                <p class="film-plot">${film.Plot}</p>
+            </div>
+        </div>
+        `
+    };
+    container.innerHTML = htmlFeed;
+};
+
+function renderEmptyWatchlist() {
+    watchContainer.innerHTML = `
+        <div class="start-exploring">
+               <p class="empty-watchlist-para">Your watchlist is looking a little empty...</p>
+               <div class="watchlist-empty-container">
+                    <div>
+                        <a href="index.html">
+                            <img src="./images/add-movies.svg">
+                        </a>    
+                    </div>
+                    <p class="watchlist-small-para">Let's add some movies!</p> 
+               </div>
+            </div>
+        `
+};
